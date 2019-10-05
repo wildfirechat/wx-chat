760397b2e64b2fb0c04226912a6cdb56a70752a4
imndx <imndxx@gmail.com>
2019-10-05 23:59:18 +0800

    friend list

--- pages/friends/friends.js
+++ pages/friends/friends.js
@@ -1,4 +1,7 @@
 // pages/friends/friends.js
+import regeneratorRuntime from '../../utils/runtime.js';
+import wfc from "../../wfc-bundle/wfc/wfc";
+import EventType from '../../wfc-bundle/wfc/wfcEvent.js';
 
 /**
  * 获取好友列表
@@ -11,28 +14,49 @@ Page({
     data: {
         friends: []
     },
+
+    onLoad(options) {
+        wfc.eventEmiter.on(EventType.UserInfoUpdate, userInfo => {
+            this.showFriendList();
+        })
+    },
     /**
      * 生命周期函数--监听页面显示
      */
     async onShow() {
-        getApp().getIMHandler().setOnReceiveMessageListener({
-            listener: (msg) => {
-                if (msg.type === 'get-friends') {
-                    this.setData({friends: msg.friends.map(item => this.createFriendItem(item))});
-                }
-            }
-        });
+        // getApp().getIMHandler().setOnReceiveMessageListener({
+        //     listener: (msg) => {
+        //         if (msg.type === 'get-friends') {
+        //             this.setData({friends: msg.friends.map(item => this.createFriendItem(item))});
+        //         }
+        //     }
+        // });
 
-        try {
-            await getApp().getIMHandler().sendMsg({
-                content: {
-                    type: 'get-friends',
-                    userId: getApp().globalData.userInfo.userId
-                }
-            });
-        } catch (e) {
-            console.log('获取好友列表失败', e);
-        }
+        // try {
+        //     await getApp().getIMHandler().sendMsg({
+        //         content: {
+        //             type: 'get-friends',
+        //             userId: getApp().globalData.userInfo.userId
+        //         }
+        //     });
+        // } catch (e) {
+        //     console.log('获取好友列表失败', e);
+        // }
+        this.showFriendList();
+    },
+
+    showFriendList() {
+        let friends = wfc.getMyFriendList(false);
+        let friendsUs = friends.map(f => {
+            let userInfo = wfc.getUserInfo(f, false);
+
+            return {
+                friendId: userInfo.uid,
+                friendHeadUrl: userInfo.portriat,
+                friendName: userInfo.displayName
+            };
+        });
+        this.setData({ friends: friendsUs });
     },
 
     createFriendItem(item) {
@@ -42,6 +66,5 @@ Page({
             friendName: item.nickName
         };
     },
-
 
 });
