fa77ed533c946a16214766499820a7701a86813a
imndx <imndxx@gmail.com>
2020-08-18 16:54:25 +0800

    update config and proto

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 export default class Config {
   // 如果需要支持音视频通话功能，必须全站使用https(包括app server和im server) + wss，
     // WebSockets over SSL/TLS
-  static USE_WSS = false;
+  static USE_WSS = true;
     // WebSocket连接端口，需要和服务端对应，不能随意修改
     static WS_PORT = 8083;
     // Secure WebSocket连接端口，需要和服务端对应，不能随意修改
     static WSS_PORT = 8084;
 
   static STATIC_RESOURCE_PATH = 'https://static.wildfirechat.cn/'
-  static APP_SERVER = 'http://wildfirechat.cn:8888';
+  static APP_SERVER = 'https://app.wildfirechat.cn';
   // 用于本地打包验证
   // static APP_SERVER = 'http://localhost:8888';
   static QR_CODE_PREFIX_PC_SESSION = "wildfirechat://pcsession/";
