0f3b2ad3dd4e71db15156a5b6e06c05897db4384
imndx <imndxx@gmail.com>
2021-02-22 17:40:04 +0800

    切换域名

--- config.js
+++ config.js
@@ -11,23 +11,23 @@ export default class Config {
   // ROUTE请求端口，默认是80；配置https时，请修改为其他端口，如443等
   static ROUTE_PORT = 443;
 
-  static STATIC_RESOURCE_PATH = 'https://static.wildfirechat.cn/'
+  static STATIC_RESOURCE_PATH = 'https://static.wildfirechat.net/'
 
   // IM SERVER的HOST，是域名或者ip，没有http等前缀!
-  static IM_SERVER_HOST = 'wildfirechat.cn';
+  static IM_SERVER_HOST = 'wildfirechat.net';
 
   //默认demo应用服务的端口是8888，地址配置为 http://${应用服务器地址}:8888 上线需要切换成https的地址。
   //static APP_SERVER = 'http://wildfirechat.cn:8888';
-  static APP_SERVER = 'https://app.wildfirechat.cn';
+  static APP_SERVER = 'https://app.wildfirechat.net';
   // 用于本地打包验证
   // static APP_SERVER = 'http://localhost:8888';
   static QR_CODE_PREFIX_PC_SESSION = "wildfirechat://pcsession/";
-  static ICE_ADDRESS = 'turn:turn.wildfirechat.cn:3478';
+  static ICE_ADDRESS = 'turn:turn.wildfirechat.net:3478';
   static ICE_USERNAME = 'wfchat';
   static ICE_PASSWORD = 'wfchat';
 
   static WX_APP_ID = 'wx_12345678';
-  static WX_APP_KEY = 'e7a9ad28474252f306c682ee78b31558cc0eca30';
+  static WX_APP_KEY = '83ae04335b791d68df7eb12df7cc732bcdc8a417';
 
   static MESSAGE_ROAMING = 1;
   // 拉取最近2小时的消息
@@ -46,7 +46,7 @@ export default class Config {
 
   // 小程序不能播放amr格式的音频，需要将amr格式转换为mp3格式
   // 本服务传入amr音频文件的地址，将音频文件转换为mp3格式，并以application/octet-stream的格式返回
-  static AMR_TO_MP3_SERVER_ADDRESS = 'https://app.wildfirechat.cn/amr2mp3?path=';
+  static AMR_TO_MP3_SERVER_ADDRESS = 'https://app.wildfirechat.net/amr2mp3?path=';
 
   // 发送消息超时时间，超时之后，认为当前连接已不可用，将进行重连，单位是秒。没有特殊需求不，不建议修改
   static SEND_MESSAGE_TIMEOUT = 20;
@@ -54,7 +54,7 @@ export default class Config {
     // 会话过期时间，表示一个会话，自己不再参与之后，多久会过期。会话列表不展示已过期的会话；单位是天
     static CONVERSATION_EXPIRE_TIME = 30;
 
-  static DEFAULT_GROUP_PORTRAIT = 'https://static.wildfirechat.cn/avatar_def.png';
+  static DEFAULT_GROUP_PORTRAIT = 'https://static.wildfirechat.net/avatar_def.png';
 
   static getWFCPlatform() {
     return 6;
