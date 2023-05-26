22fc9311111b6de47a9865dd03250bd0732f2dd3
x86 <1960867973@qq.com>
2023-05-26 11:34:27 +0800

    更新配置

--- pages/voip/voip.js
+++ pages/voip/voip.js
@@ -15,18 +15,15 @@ Page({
     let shortLinkInfo = wfc.getShortLinkInfo();
 
     // 本地调试
-    const voipBaseWebUrl = 'https://192.168.2.180:8080';
+    // const voipBaseWebUrl = 'https://192.168.2.180:8080';
 
     // 多人版音视频
-    // const voipBaseWebUrl = 'https://static.wildfirechat.cn/voip-multi-1684984765.html';
+    const voipBaseWebUrl = 'https://static.wildfirechat.cn/voip-multi-1685070010.html';
     // 高级版音视频
-    //const voipBaseWebUrl = 'https://static.wildfirechat.cn/voip-conference-1684984633.html';
+    //const voipBaseWebUrl = 'https://static.wildfirechat.cn/voip-conference-1685069872.html';
 
     let token = shortLinkInfo.token;
-    console.log('token o', token);
-
     token = token.replaceAll('+', '.').replaceAll('/', '_').replaceAll('=', '-')
-    console.log('token r', token);
     let voipWebUrl = `${voipBaseWebUrl}?type=${options.type}&authToken=${authToken}&token=${token}&clientId=${shortLinkInfo.clientId}&server=${encodeURIComponent(shortLinkInfo.server)}&debug=true`;
 
     console.log('start voip page', shortLinkInfo,  voipWebUrl);
