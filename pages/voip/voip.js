98f399cadd344fab637bc0a48adbcfd1b50923db
x86 <1960867973@qq.com>
2023-05-27 00:04:22 +0800

    update avenginekitproxy

--- pages/voip/voip.js
+++ pages/voip/voip.js
@@ -15,16 +15,16 @@ Page({
     let shortLinkInfo = wfc.getShortLinkInfo();
 
     // 本地调试
-    // const voipBaseWebUrl = 'https://192.168.2.180:8080';
+    const voipBaseWebUrl = 'https://voip.wfim.work';
 
     // 多人版音视频
     // const voipBaseWebUrl = 'https://static.wildfirechat.cn/voip-multi-1685070010.html';
     // 高级版音视频
-    const voipBaseWebUrl = 'https://static.wildfirechat.cn/voip-conference-1685069872.html';
+    // const voipBaseWebUrl = 'https://static.wildfirechat.cn/voip-conference-1685069872.html';
 
     let token = shortLinkInfo.token;
     token = token.replaceAll('+', '.').replaceAll('/', '_').replaceAll('=', '-')
-    let voipWebUrl = `${voipBaseWebUrl}?type=${options.type}&authToken=${authToken}&token=${token}&clientId=${shortLinkInfo.clientId}&server=${encodeURIComponent(shortLinkInfo.server)}&debug=false`;
+    let voipWebUrl = `${voipBaseWebUrl}?type=${options.type}&authToken=${authToken}&token=${token}&clientId=${shortLinkInfo.clientId}&server=${encodeURIComponent(shortLinkInfo.server)}&debug=true`;
 
     console.log('start voip page', shortLinkInfo,  voipWebUrl);
     this.setData({url: voipWebUrl});
